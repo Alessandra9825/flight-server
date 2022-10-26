7285828a9baedd22832b3e7053869e101994b912
Alessandra9825 <alessandra.guedes@outlook.com>
2022-10-26 09:17:59 -0300

    Insert Secrete Manager AWS

--- flighty.py
+++ flighty.py
@@ -2,15 +2,70 @@ from flask import Flask
 from flaskext.mysql import MySQL
 from flask import Flask, request, render_template, jsonify, redirect, url_for, session, flash, g
 from werkzeug.security import check_password_hash, generate_password_hash
+from waitress import serve
 import functools
+import boto3
+import base64
+from botocore.exceptions import ClientError
+import json
+from types import SimpleNamespace
 
+
+def get_secret():
+
+    secret_name = "Masteraccoundb"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            decoded_binary_secret = base64.b64decode(
+                get_secret_value_response['SecretBinary'])
+
+    return secret
+
+
+dados = json.loads(get_secret(), object_hook=lambda d: SimpleNamespace(**d))
 
 mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'AlU1998?'
+app.config['MYSQL_DATABASE_PASSWORD'] = dados.password
 app.config['MYSQL_DATABASE_DB'] = 'flight'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = dados.host
 app.config.update(SECRET_KEY='dev')
 mysql.init_app(app)
 conn = mysql.connect()
@@ -345,7 +400,6 @@ def print():
 
 
 if __name__ == "__main__":
-    from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
 
 
